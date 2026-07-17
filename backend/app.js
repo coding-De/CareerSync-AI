ee4818abe6f4396110a7705a4558a5238a88c72c
omkar <omkarramtripathi@gmail.com>
2026-07-17 07:48:56 +0530

    update CORS origin and service path for production environment

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://careersync-ai-1-0yv2.onrender.com", credentials: true }));
 
 
 dbconnect();
