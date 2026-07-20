b20de823e2fdefc53d29c5ff7bc820d7898cfbbc
coding-De <142860346+coding-De@users.noreply.github.com>
2026-07-20 08:21:09 +0530

    Enhance token retrieval from cookies and headers

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -3,7 +3,9 @@ const tokenblacklistModel = require("../Models/blacklist.model");
 
 
 const authUser = async (req, res, next) => {
-    const token = req.cookies.token;
+    const token = 
+      req.cookies?.token || 
+      req.headers.authorization?.split(" ")[1];
 
     if (!token) {
        return res.status(401).json({
